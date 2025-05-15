73e14d7b58d5955a204c45740927aea982482fdb
Jeanne Endres <jeanne.endres@gmx.de>
2025-05-15 14:24:17 -0400

    add loader compas

--- src/data_loading.py
+++ src/data_loading.py
@@ -1,6 +1,6 @@
 import pandas as pd
-from aif360.datasets import AdultDataset
-from src.data_preprocessing import preprocessing_adult
+from aif360.datasets import AdultDataset, CompasDataset
+from src.data_preprocessing import preprocessing_adult, preprocessing_compas
 
 def load_adult_sex():
     ad = AdultDataset(
@@ -25,7 +25,7 @@ def load_adult_sex():
     df = pd.DataFrame(ad.features, columns=ad.feature_names)
     df['label'] = ad.labels.ravel()
     
-    return df
+    return ad, df
 
 
 def load_adult_race():
@@ -51,4 +51,48 @@ def load_adult_race():
     df = pd.DataFrame(ad.features, columns=ad.feature_names)
     df['label'] = ad.labels.ravel()
     
-    return df
+    return ad, df
+
+def load_compas_sex(custom_preprocessing=preprocessing_compas):
+    ds = CompasDataset(
+        protected_attribute_names=['sex'],
+        privileged_classes=[['Male']],
+        features_to_drop=['age_cat'],
+        categorical_features=[
+            'race', 'c_charge_degree', 'c_charge_desc'
+        ],
+        custom_preprocessing=custom_preprocessing
+    )
+    df = pd.DataFrame(ds.features, columns=ds.feature_names)
+    df['label'] = ds.labels.ravel()
+    df['race']  = ds.protected_attributes[:, 0]
+
+    df.drop(    
+        ['c_charge_desc=Other', 'c_charge_degree=F'],
+        axis=1,
+        inplace=True
+    )
+
+    return ds, df
+
+def load_compas_race(custom_preprocessing=preprocessing_compas):
+    ds = CompasDataset(
+        protected_attribute_names=['race'],
+        privileged_classes=[['Caucasian']],
+        features_to_drop=['age_cat'],
+        categorical_features=[
+            'sex', 'c_charge_degree', 'c_charge_desc'
+        ],
+        custom_preprocessing=custom_preprocessing
+    )
+    df = pd.DataFrame(ds.features, columns=ds.feature_names)
+    df['label'] = ds.labels.ravel()
+    df['race']  = ds.protected_attributes[:, 0]
+
+    df.drop(    
+        ['c_charge_desc=Other', 'sex=Female', 'c_charge_degree=F'],
+        axis=1,
+        inplace=True
+    )
+
+    return ds, df
